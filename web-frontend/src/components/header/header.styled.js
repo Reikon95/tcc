ffe1e6d1e122c752e20dede3a319971fd3aa3720
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-08-10 18:05:45 +0100

    styling fixes

--- web-frontend/src/components/header/header.styled.js
+++ web-frontend/src/components/header/header.styled.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const StyledHeader = styled.h1`
     padding: 0.1em;
     display: block;
-    font-size: 4em;
+    font-size: 3em;
     text-align: center;
 `
