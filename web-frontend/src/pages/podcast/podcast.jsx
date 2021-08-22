3e3df3de5dc08185fdd554803dc9b82699ada25e
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-08-22 16:53:21 +0100

    podcast page

---
+++ web-frontend/src/pages/podcast/podcast.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Podcast() {
+    // https://feeds.captivate.fm/thecodercareer/
+    return (
+        <>
+        TCC Podcast
+        Subscribe to the TCC Podcast!
+        </>
+    )
+}
