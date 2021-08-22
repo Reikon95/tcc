1a914b4dda18a22bedfe0b49762a41fa6b1aa232
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-08-22 22:24:42 +0100

    font adjustments

--- web-frontend/src/pages/landing/landing.jsx
+++ web-frontend/src/pages/landing/landing.jsx
@@ -11,71 +11,7 @@ export default function Landing() {
         <h3>Resouces to break into, and level up in, the tech industry.</h3>
       </div>
 
-      <div class="container">
-        <div class="box">
-          <div class="flip-box">
-            <div class="flip-box-inner">
-              <div class="flip-box-front">
-                <div className="inner-text">
-                  {" "}
-                  <a
-                    href="https://www.youtube.com/channel/UC-m9HBSgx5drU4cqfuNg5vA"
-                    target="_blank"
-                  >
-                    Youtube
-                  </a>
-                </div>
-              </div>
-              <div class="flip-box-back">
-                <a
-                  href="https://www.youtube.com/channel/UC-m9HBSgx5drU4cqfuNg5vA"
-                  target="_blank"
-                >
-                  Check out the latest uploads...
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="flip-box">
-            <div class="flip-box-inner">
-              <div class="flip-box-front">
-                <div className="inner-text">Jobs</div>
-              </div>
-              <div class="flip-box-back">
-                {" "}
-                <Link to="/jobs" className="navLink">
-                  Latest Vacancies
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>{" "}
-        <div class="box">
-          <div class="flip-box">
-            <div class="flip-box-inner">
-              <div class="flip-box-front">
-                <div className="inner-text">Podcast</div>
-              </div>
-                              <Link to="/podcast" className="navLink">
 
-              <div class="flip-box-back">TCC Podcast</div>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div class="box">
-          <div class="flip-box">
-            <div class="flip-box-inner">
-              <div class="flip-box-front">
-                <div className="inner-text">Newsletter</div>
-              </div>
-              <div class="flip-box-back">Coming soon!</div>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
