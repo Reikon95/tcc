5caf928760725e9451e5efb26f71dead6ed318ec
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-08-22 22:15:35 +0100

    added iframe for substack support

--- web-frontend/src/pages/newsletter/newsletter.jsx
+++ web-frontend/src/pages/newsletter/newsletter.jsx
@@ -1,24 +1,21 @@
 import { Container } from '@material-ui/core';
 import React from 'react'
 import Header from "../../components/header/header";
-
+import './newsletter.css'
 export default function NewsLetter() {
     const signupIframeStyle = {
         border: '1px solid #EEE',
-        background: 'white'
+        background: 'transparent'
     }
     return (
         <>
             <Header text="Newsletter"/>
             <Container>
             <div>
-                If you want to get the latest tips, tricks and vacancies straight into your inbox, subscribe to our newsletter: 
+                If you want to get the latest tips, tricks and vacancies straight into your inbox, subscribe to our newsletter, coming soon!
             </div>
-            <div>
+            <div className='subscribe-wrapper'>
 <iframe src="https://thecodercareer.substack.com/embed" width="480" height="320" style={signupIframeStyle} frameborder="0" scrolling="no"></iframe>
-            </div>
-            <div>
-                Interested in sponsoring our newsletter or featuring your job vacancy? Contact us.
             </div>
         
             </Container>
