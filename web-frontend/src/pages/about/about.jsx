7743da3a68af3a71e9549b01d83e666b300dcf5e
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-08-23 20:44:59 +0100

    contact page

--- web-frontend/src/pages/about/about.jsx
+++ web-frontend/src/pages/about/about.jsx
@@ -3,7 +3,7 @@ import Header from "../../components/header/header";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import "./about.css"
+import "./about.css";
 export default function About() {
   return (
     <>
@@ -11,9 +11,10 @@ export default function About() {
 
       <Container className="about-us-content">
         <div className="intro-text">
-          The Coder Career is a hub for for software professionals to break into, level up and innovate in the technology
-          industry. We feature job advertisments,
-          interesting tech stories and useful content for careers in tech.
+          The Coder Career is a hub for for software professionals to break
+          into, level up and innovate in the technology industry. We feature job
+          advertisments, interesting tech stories and useful content for careers
+          in tech.
         </div>
         <div>
           The Coder Career was founded by Cam Blackwood in 2020. Cam worked as a
@@ -27,14 +28,23 @@ export default function About() {
           into the wonderful world of technology, and we want to give them all
           the help we can!
         </div>
-        <div>
-          Contact Cam
-          <img src='https://avatars.githubusercontent.com/u/38852603?v=4' width='200' height='200' alt='picture of cam' className='cam-picture'/>
-          cameronblackwood.developer@gmail.com
-          cameronblackwood.com
+        Contact Cam
+        <div className="contact-panel">
+          <img
+            src="https://avatars.githubusercontent.com/u/38852603?v=4"
+            width="200"
+            height="200"
+            alt="picture of cam"
+            className="cam-picture"
+          />
+          <div>
+            I'm currently looking for partnerships for The Coder Career to help
+            establish it as a careers hub for technology professionals
+            worldwide. If you think you can help, please get in touch!
+          </div>
+          <div>emailicon githubicon websiteicon linkedinicon</div>
         </div>
       </Container>
     </>
   );
 }
-
